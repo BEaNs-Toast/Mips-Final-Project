4d12381a5ee86dc2fc7a3c4d3d1b966382c7783c
BEaNs-Toast <bmtzobery@cpp.edu>
2025-04-28 12:15:52 -0700

    Added commets to help understand code

--- Final_Project.asm
+++ Final_Project.asm
@@ -1,52 +1,59 @@
-.macro set_brush(%int)
+.macro set_brush(%int)#This code places the brush in a specific location on the screen
 	li $t4, %int
 	add $t6, $t0, $t4
 	add $t7, $s0, $t4
 .end_macro
-.macro movebrush
+.macro movebrush #this macro moves the brush by 1 pixel to the right on the screen
 	addi $t4, $t4, 4
     	add $t6, $t0, $t4
     	add $t7, $s0, $t4
 .end_macro
-.macro paint
+.macro paint #this macro changes the color of the screen by what color was inputed into $t1
 	sw $t1, 0($t6)
 	li $t2, 1
-    	#sw $t2, 0($t7)
+    	#sw $t2, 0($t7) This code does not work, but what it should do is tell the computer that that section of the screen is occpuied
 .end_macro
 
 .data
-DISPLAY: .word 0x10008000
-KEY_PRESS: .word 0xFFFF0000
+DISPLAY: .word 0x10008000 #Display input
+KEY_PRESS: .word 0xFFFF0000 #Keyboard output 
 D_key: .asciiz "Block goes right"
 A_key: .asciiz "Block goes Left"
 S_key:	.asciiz "Block snaps to bottom"
 R_key: 	.asciiz "Block Rotates"
-DIS_ARR: .byte 0:2048
-BORDER: .word 0x444444
-GREY_PATTERN: .word 0x161616 
-BLACK: .word 0x000000
+DIS_ARR: .byte 0:2048 #supposed to stroe digits, is not used currently
+BORDER: .word 0x444444 #Border color
+GREY_PATTERN: .word 0x161616 #grey Color
+BLACK: .word 0x000000 #Black Color
+#Before starting the code, make sure to set the bitmap display to this:
+# Unit Width in Pixles - 16
+#Unit Height in Pixes - 16
+# Display Width in Pixels - 156
+#Display Height in Pixels - 512
+#Base Adress for display - 0x10008000 ($gp)
 
+#Make sure to connect both the bitmap display and the Keyboard and Display MMIO Simulator as well.
 
 .text
 	main:
 	lw $t0, DISPLAY
 	la $s0, DIS_ARR
 	set_brush(0)
-	jal draw_sides
+	jal draw_sides #addes the sides for the screen
 	set_brush(1920)
-	jal draw_bottom
+	jal draw_bottom #addes the bottom for the screen
 	set_brush(4)
 	lw $t9, KEY_PRESS
 	lw $t8, 0($t9)
-	beq $t8, 1, check_key
+	beq $t8, 1, check_key 
 	j main
 check_key:
-	lw $a0, 4($t9)
+	lw $a0, 4($t9) #this loads the character pressed into $a0
 	beq $a0, 100, D_press
 	beq $a0, 114, R_press
 	beq $a0, 97, A_press
 	beq $a0, 115, S_press
-	beq $a0, 113, end
+	beq $a0, 113, end #If you press Q, this activates
 	j main
 draw_sides:
 	la $t5, BORDER
